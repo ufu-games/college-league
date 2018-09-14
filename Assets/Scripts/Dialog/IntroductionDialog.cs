0c5fcac1f825ab7879fef8bebc9b08c79f52ecd0
Guilherme Oliveira <guilherme.061@gmail.com>
2018-09-14 11:25:04 -0300

    Dialogo Introducao

---
+++ Assets/Scripts/Dialog/IntroductionDialog.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroductionDialog : MonoBehaviour {
+
+	[Header("Sons Auxiliares")]
+	public AudioClip messageBeep;
+	public AudioClip shockSfx;
+	private int m_currentState = 0;
+	private AudioSource m_myAudioSource;
+	bool m_canPress;
+
+	void Start () {
+		// StartCoroutine(PlayDialog());
+		m_myAudioSource = GetComponent<AudioSource>();
+		EvaluateState(m_currentState);
+	}
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.Space) && m_canPress) {
+			m_currentState++;
+			EvaluateState(m_currentState);
+		}
+	}
+
+	private void EvaluateState(int state) {
+		switch(state) {
+			case 0:
+				StartCoroutine(RenderPlace("13 de Setembro de 2023\nQuarto da Damires"));
+			break;
+			case 1:
+				StartCoroutine(RenderDialog("Ah... O que eu não faria para ter um Sugar Daddy igual ao Roberval."));
+			break;
+			case 2:
+				StartCoroutine(RenderDialog("Eu não aguento mais perder minha série só para ouvir 'Você só fez sua obrigação'..."));
+			break;
+			case 3:
+				WriteText.instance.ActivatePanels(false, false);
+				SoundManager.instance.PlaySfx(messageBeep);
+			break;
+			case 4:
+				StartCoroutine(RenderDialog("EITA! O Pabllo Vittar liberou o Lula da prisão junto da Gretchen."));
+			break;
+			case 5:
+				StartCoroutine(RenderDialog("Agora ele foi longe demais!!!"));
+			break;
+			case 6:
+				WriteText.instance.ActivatePanels(false, false);
+				SoundManager.instance.PlaySfx(messageBeep);
+			break;
+			case 7:
+				Camera.main.GetComponent<Screenshake>().ShakeScreen(0.1f);
+				m_myAudioSource.PlayOneShot(shockSfx);
+				StartCoroutine(RenderDialog("Yuke!!!"));
+			break;
+			case 8:
+				StartCoroutine(ReprovouRoutine());
+			break;
+			case 9:
+				StartCoroutine(RenderDialog("Nao acredito que vou ter que ir chorar nota..."));
+			break;
+			case 10:
+				StartCoroutine(RenderDialog("Vou ter que enfrentar meu pior inimigo."));
+			break;
+			case 11:
+				Camera.main.GetComponent<Screenshake>().ShakeScreen(0.1f);
+				m_myAudioSource.PlayOneShot(shockSfx);
+				StartCoroutine(RenderDialog("O Sol das 15."));
+			break;
+			default:
+				WriteText.instance.ActivatePanels(false, false);
+			break;
+		}
+	}
+
+	private IEnumerator ReprovouRoutine() {
+		m_canPress = false;
+
+		Camera.main.GetComponent<Screenshake>().ShakeScreen(0.2f);
+		m_myAudioSource.PlayOneShot(shockSfx);
+		yield return StartCoroutine(WriteText.instance.RenderTextRoutine("ESSE PAQUIDERME ME REPROVOU"));
+		yield return new WaitForSeconds(0.5f);
+		Camera.main.GetComponent<Screenshake>().ShakeScreen(0.1f);
+		m_myAudioSource.PlayOneShot(shockSfx);
+		yield return StartCoroutine(WriteText.instance.RenderTextRoutine("POR"));
+		yield return new WaitForSeconds(0.5f);
+		Camera.main.GetComponent<Screenshake>().ShakeScreen(0.1f);
+		m_myAudioSource.PlayOneShot(shockSfx);
+		yield return StartCoroutine(WriteText.instance.RenderTextRoutine("1 (UM)"));
+		yield return new WaitForSeconds(0.5f);
+		Camera.main.GetComponent<Screenshake>().ShakeScreen(0.1f);
+		m_myAudioSource.PlayOneShot(shockSfx);
+		yield return StartCoroutine(WriteText.instance.RenderTextRoutine("PONTO!"));
+		yield return new WaitForSeconds(0.5f);
+
+		m_canPress = true;
+	}
+	private IEnumerator RenderPlace(string text) {
+		m_canPress = false;
+		yield return StartCoroutine(WriteText.instance.RenderPlaceRoutine(text));
+		m_canPress = true;
+	}
+
+	private IEnumerator RenderDialog(string text) {
+		m_canPress = false;
+		yield return StartCoroutine(WriteText.instance.RenderTextRoutine(text));
+		m_canPress = true;
+	}
+
+	private IEnumerator PlayDialog() {
+		yield return null;
+		// yield return StartCoroutine(RenderTextRoutine("Vou ter que enfrentar meu pior inimigo!"));
+		// yield return StartCoroutine(RenderTextRoutine("O Sol das 15."));
+	}
+}
---
+++ Assets/Scripts/camera/Screenshake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshake : MonoBehaviour {
+
+	public float screenShakeDecay = .5f;
+	private Vector3 m_screenShake;
+	private float m_screenShakeAmount;
+
+	void Update () {
+		if(m_screenShakeAmount > 0) {
+			float t_x = Random.Range(-m_screenShakeAmount, m_screenShakeAmount);
+			float t_y = Random.Range(-m_screenShakeAmount, m_screenShakeAmount);
+			m_screenShake = new Vector3(t_x,
+										t_y,
+										0f);
+			m_screenShakeAmount -= Time.deltaTime * screenShakeDecay;
+		} else {
+			m_screenShake = Vector3.zero;
+			transform.position = new Vector3(0f, 0f, -10f);
+		}
+
+		transform.position += m_screenShake;
+	}
+
+	public void ShakeScreen(float toShake) {
+		m_screenShakeAmount = toShake;
+	}
+}
